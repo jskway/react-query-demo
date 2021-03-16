fbc86c7752bcadea0a797ea77b7be5df7749aa33
Jack Kim <jack@jackkim.dev>
2021-03-15 21:59:41 -0700

    Add People and Planet components with useQuery

--- src/components/Planets.js
+++ src/components/Planets.js
@@ -1,9 +1,36 @@
 import React from 'react'
+import { useQuery } from 'react-query'
+import Planet from './Planet'
+
+const fetchPlanets = async () => {
+  const res = await fetch('http://swapi.dev/api/planets/')
+  return res.json()
+}
 
 const Planets = () => {
+  // data is the result of fetchPlanets()
+  // status is the status of the request - eg. 'loading' 'error' 'success'
+  const { data, status } = useQuery('planets', fetchPlanets)
+  console.log(data)
   return (
     <div>
       <h2>Planets</h2>
+      {/*<p>{ status }</p>*/}
+
+      {status === 'loading' && (
+        <div>Loading data...</div>
+      )}
+
+      {status === 'error' && (
+        <div>Error fetching data</div>
+      )}
+
+      {status === 'success' && (
+        <div>
+          { data.results.map(planet => <Planet key={planet.name} planet={planet} />)}
+        </div>
+      )}
+
     </div>
   )
 }
